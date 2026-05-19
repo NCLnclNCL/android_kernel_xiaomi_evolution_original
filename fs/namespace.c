28428a60b4c634eebfd0dee974cc2e88022fc1c7
NCLnclNCL <126905141+NCLnclNCL@users.noreply.github.com>
2026-05-19 22:46:34 +0700

    remove mnt backup

--- fs/namespace.c
+++ fs/namespace.c
@@ -165,30 +165,12 @@ static void mnt_free_id(struct mount *mnt)
 {
 	int id = mnt->mnt_id;
 #if defined(CONFIG_KSU_SUSFS_SUS_MOUNT)
-	int mnt_id_backup = mnt->mnt.susfs_mnt_id_backup;
-	// We should first check the 'mnt->mnt.susfs_mnt_id_backup', see if it is DEFAULT_SUS_MNT_ID_FOR_KSU_PROC_UNSHARE
-	// if so, these mnt_id were not assigned by mnt_alloc_id() so we don't need to free it.
-	if (unlikely(mnt_id_backup == DEFAULT_SUS_MNT_ID_FOR_KSU_PROC_UNSHARE)) {
-		return;
-	}
 	// Now we can check if its mnt_id is sus
 	if (unlikely(mnt->mnt_id >= DEFAULT_SUS_MNT_ID)) {
 		spin_lock(&mnt_id_lock);
 		ida_remove(&susfs_mnt_id_ida, id);
 		if (susfs_mnt_id_start > id)
 			susfs_mnt_id_start = id;
-		spin_unlock(&mnt_id_lock);
-		return;
-	}
-	// Lastly if 'mnt->mnt.susfs_mnt_id_backup' is not 0, then it contains a backup origin mnt_id
-	// so we free it in the original way
-	if (likely(mnt_id_backup)) {
-		// If mnt->mnt.susfs_mnt_id_backup is not zero, it means mnt->mnt_id is spoofed,
-		// so here we return the original mnt_id for being freed.
-		spin_lock(&mnt_id_lock);
-		ida_remove(&mnt_id_ida, mnt_id_backup);
-		if (mnt_id_start > mnt_id_backup)
-			mnt_id_start = mnt_id_backup;
 		spin_unlock(&mnt_id_lock);
 		return;
 	}
@@ -1138,7 +1120,7 @@ vfs_kern_mount(struct file_system_type *type, int flags, const char *name, void 
                 current->comm,
                 flags,
                 susfs_is_current_ksu_domain());
-		mnt = alloc_vfsmnt(name, false, 0);
+		mnt = alloc_vfsmnt(name, true, 0);
 		goto bypass_orig_flow;
 	}
 	mnt = alloc_vfsmnt(name, false, 0);
@@ -1231,17 +1213,6 @@ static struct mount *clone_mnt(struct mount *old, struct dentry *root,
                         current->comm);
 			mnt = alloc_vfsmnt(old->mnt_devname, true, 0);
 			goto bypass_orig_flow;
-		}
-		// if it is doing unshare
-		mnt = alloc_vfsmnt(old->mnt_devname, true, old->mnt_id);
-		if (mnt) {
-			                pr_info("susfs: clone_mnt_1 dev=%s mnt_id=%d pid=%d comm=%s\n",
-                        old->mnt_devname ?
-                                old->mnt_devname : "NULL",
-                        old->mnt_id,
-                        current->pid,
-                        current->comm);
-			mnt->mnt.susfs_mnt_id_backup = DEFAULT_SUS_MNT_ID_FOR_KSU_PROC_UNSHARE;
 		}
 		goto bypass_orig_flow;
 	}
