e4f5199f898a3533838770fd9e15e5617a34b896
NCLnclNCL <126905141+NCLnclNCL@users.noreply.github.com>
2026-05-21 16:09:34 +0700

    fix type

--- fs/namespace.c
+++ fs/namespace.c
@@ -1109,9 +1109,13 @@ vfs_kern_mount(struct file_system_type *type, int flags, const char *name, void 
 	if (!type)
 		return ERR_PTR(-ENODEV);
 
-#if defined(CONFIG_KSU_SUSFS_SUS_MOUNT)
+#ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
 	// For newly created mounts, the only caller process we care is KSU
-	mnt = alloc_vfsmnt(name, true, 0);
+	if (!susfs_is_sdcard_android_data_decrypted && unlikely(susfs_is_current_ksu_domain())) {
+		mnt = alloc_vfsmnt(name, true, 0);
+		goto bypass_orig_flow;
+	}
+	mnt = alloc_vfsmnt(name, false, 0);
 bypass_orig_flow:
 #else
 	mnt = alloc_vfsmnt(name);
